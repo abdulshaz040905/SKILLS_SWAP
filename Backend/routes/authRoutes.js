96d7f96d591b4e446135307a91b37dc64b11f6df
Abdul shaz <abdulshaz040905@gmail.com>
2026-04-12 22:48:28 +0530

    Added login Route

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -37,4 +37,49 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN API
+const jwt = require("jsonwebtoken");
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // Compare entered password with hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      { userId: user._id },
+      "mysecretkey",
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message
+    });
+  }
+});
+
 module.exports = router;
