688c4496baca7a68c2299d7561e3358cff8c2b85
Abdul shaz <abdulshaz040905@gmail.com>
2026-04-15 14:50:28 +0530

    Added Swap Request API

---
+++ Backend/routes/swapRoutes.js
@@ -0,0 +1,83 @@
+// /backend/routes/swapRoutes.js
+
+const express = require("express");
+const verifyToken = require("../middleware/verifyToken");
+const SwapRequest = require("../models/SwapRequest");
+
+const router = express.Router();
+
+// SEND SWAP REQUEST
+router.post("/send/:receiverId", verifyToken, async (req, res) => {
+  try {
+    const senderId = req.user.userId;
+    const receiverId = req.params.receiverId;
+
+    // Prevent sending request to self
+    if (senderId === receiverId) {
+      return res.status(400).json({
+        message: "You cannot send request to yourself"
+      });
+    }
+
+    // Check if request already exists
+    const existingRequest = await SwapRequest.findOne({
+      sender: senderId,
+      receiver: receiverId,
+      status: "pending"
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({
+        message: "Request already sent"
+      });
+    }
+
+    const swapRequest = await SwapRequest.create({
+      sender: senderId,
+      receiver: receiverId
+    });
+
+    res.status(201).json({
+      message: "Swap request sent successfully",
+      swapRequest
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message
+    });
+  }
+});
+
+// GET MY SWAP REQUESTS
+router.get("/my-requests", verifyToken, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const incoming = await SwapRequest.find({
+      receiver: userId
+    })
+      .populate("sender", "name email skillsOffered skillsWanted")
+      .sort({ createdAt: -1 });
+
+    const outgoing = await SwapRequest.find({
+      sender: userId
+    })
+      .populate("receiver", "name email skillsOffered skillsWanted")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      incoming,
+      outgoing
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message
+    });
+  }
+});
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 //All Routes
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const swapRoutes = require("./routes/swapRoutes");
 
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/swaps", swapRoutes);
 
 app.get("/", (req, res) => {
   res.send("SkillSwap API is running...");
