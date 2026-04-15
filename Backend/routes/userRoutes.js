f3bf4822d3e1006a0464932d042c26c5eaa5c55d
Abdul shaz <abdulshaz040905@gmail.com>
2026-04-15 14:36:51 +0530

    Added Mathcing Routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -32,4 +32,28 @@ router.put("/profile", verifyToken, async (req, res) => {
   }
 });
 
+// FIND MATCHING USERS
+router.get("/matches", verifyToken, async (req, res) => {
+  try {
+    // Get current logged-in user
+    const currentUser = await User.findById(req.user.userId);
+
+    const matches = await User.find({
+      _id: { $ne: currentUser._id },
+
+      skillsOffered: { $in: currentUser.skillsWanted },
+
+      skillsWanted: { $in: currentUser.skillsOffered }
+    }).select("-password");
+
+    res.status(200).json(matches);
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message
+    });
+  }
+});
+
 module.exports = router;
