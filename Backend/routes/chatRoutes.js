2db162a101267aab209b793259120c5325fe234e
Abdul shaz <abdulshaz040905@gmail.com>
2026-04-16 15:41:26 +0530

    Video call API Integrated

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -60,4 +60,43 @@ router.get("/history/:roomId", verifyToken, async (req, res) => {
   }
 });
 
+const verifyPremium = require("../middleware/verifyPremium");
+
+// VIDEO CALL ACCESS
+router.get(
+  "/video-call/:otherUserId",
+  verifyToken,
+  verifyPremium,
+  async (req, res) => {
+    try {
+      const currentUserId = req.user.userId;
+      const otherUserId = req.params.otherUserId;
+
+      const acceptedSwap = await SwapRequest.findOne({
+        status: "accepted",
+        $or: [
+          { sender: currentUserId, receiver: otherUserId },
+          { sender: otherUserId, receiver: currentUserId }
+        ]
+      });
+
+      if (!acceptedSwap) {
+        return res.status(403).json({
+          message: "Video call allowed only for accepted swaps"
+        });
+      }
+
+      res.status(200).json({
+        message: "Video call access granted"
+      });
+
+    } catch (error) {
+      res.status(500).json({
+        message: "Server Error",
+        error: error.message
+      });
+    }
+  }
+);
+
 module.exports = router;
